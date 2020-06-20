a2cb9b9e136b47e6ddaf5aaf51c83d79e5755a71
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-20 00:49:42 -0400

    Ajax openweather API

--- javascript.js
+++ javascript.js
@@ -1,7 +1,19 @@
-var yorkRegion = ["Georgina", "East Gwillimbury", "King", "Vaughan", "Whitechurch Stouville", "Markham"];
+var yorkRegion = ["Georgina", "East Gwillimbury", "King", "Vaughan", "Whitechurch Stouville", "Markham", "Toronto"];
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + yorkRegion[6] + ",ca&appid=02c767f928e7e5ad4f0e01b6982bd3e6"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
 
 $('aside').append('<p>Search for a City</p>')
-$('aside').append('<div class="input-group mb-3"><input type="text" class="form-control" aria-describedby="button-addon2"><div class="input-group-append"><button class="btn btn-primary" type="button" id="button-addon2">Button</button></div></div>')
+$('aside').append('<div id="search-city" class="input-group mb-3"></div>')
+$('#search-city').append('<input type="text" class="form-control" aria-describedby="button-addon2">')
+$('#search-city').append('<div id="button" class="input-group-append"></div>')
+$('#button').append('<button class="btn btn-primary" type="button" id="button-addon2">Button</button>');
 for (var i = 0; i < yorkRegion.length; i++) {
     $('aside').append('<button class="btn btn-light border border-secondary" href="#' + yorkRegion[i] + '">' + yorkRegion[i] + '</button>');
 }
