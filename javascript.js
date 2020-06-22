11881c1449a8d27f5e26fd142d0ba3ec2c53c8e1
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-22 07:16:12 -0400

    Added geolocation API

--- javascript.js
+++ javascript.js
@@ -89,6 +89,7 @@ function fiveDayForecast(newCity) {
       $('#blue-' + i).append('<p>Humidity: ' + response['list'][i * 8]['main']['humidity'] + '%</p>')
     }
   });
+  geolocation()
 }
 
 function searchHistory (newCity) {
@@ -110,3 +111,25 @@ function searchHistory (newCity) {
   $('aside, #current-weather, #forecast').empty()
 }
 
+function geolocation () {
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    var crd = pos.coords;
+  
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
+}
